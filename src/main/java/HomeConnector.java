a48b68dce0cd6b3907f275dae9bd9c8d0b66eb5e
harv <harveystephen.guimalan@gmail.com>
2024-11-16 20:07:49 +0800

    added java

---
+++ src/main/java/HomeConnector.java
@@ -0,0 +1,43 @@
+
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Servlet implementation class HomeConnector
+ */
+@WebServlet("/HomeConnector")
+public class HomeConnector extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HomeConnector() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("GET METHOD CALLED");
+		 RequestDispatcher dispatcher = request.getRequestDispatcher("home.jsp");
+	     dispatcher.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,13 @@
+<% response.sendRedirect("/vrum/home"); %>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Vrum</title>
+</head>
+<body>
+	<p>This is index</p>
+</body>
+</html>
